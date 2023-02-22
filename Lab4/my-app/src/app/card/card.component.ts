85b2a7d009370d4b8fdb9f75500617e07a89be73
бог <sitasieday@gmail.com>
2023-02-22 22:23:18 +0600

    lab4

---
+++ Lab4/my-app/src/app/card/card.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core'
+import {ICard} from "../data/cards";
+
+@Component({
+  templateUrl: './card.component.html',
+  selector: 'app-card',
+  styleUrls: ['./card.component.css'],
+  interpolation: ['{{', '}}']
+})
+export class CardComponent {
+  @Input() card: ICard
+
+  details = false
+
+  shareOnWhatsapp(card: ICard) {
+    const message = `Check out this product: ${card.title} for ${card.price}₸ at ${card.link}`;
+    const url = `whatsapp://send?text=${encodeURIComponent(message)}`;
+    window.location.href = url;
+  }
+
+  shareOnTelegram(card: ICard) {
+    const message = `Check out this product: ${card.title} for ${card.price}₸ at ${card.link}`;
+    const url = `tg://msg?text=${encodeURIComponent(message)}`;
+    window.location.href = url;
+  }
+}
